fcb89a2886e24d89e81343f595a8ed00753dee35
Chinara0303 <chinaraei@code.edu.az>
2024-02-25 17:43:22 +0400

    Completed solutions section

---
+++ project-2/assets/js/tab-menu.js
@@ -0,0 +1,20 @@
+
+// tab menu
+let tabMenus = document.querySelectorAll(".tab-menu-heading");
+let contents = document.querySelectorAll("#demand-skills .content");
+
+for (var tabMenu of tabMenus) {
+    tabMenu.addEventListener("click", function () {
+        let index = this.getAttribute("data-index");
+        document.querySelector(".tab-menu .active").classList.remove("active");
+        this.classList.add("active");
+       
+        for (let content of contents) {
+            content.classList.add("de-active-content");
+            if (content.getAttribute("data-index") === index) {
+                content.classList.remove("de-active-content");
+                content.style.transition = ".3s linear"
+            }
+        }
+    });
+}
